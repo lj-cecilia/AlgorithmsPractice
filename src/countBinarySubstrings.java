600fa08ba6a5509e48f6085a00addc79bf82492a
cecilia <yijialiu523@gmail.com>
2023-10-05 03:16:42 -0400

    daily practice

---
+++ src/countBinarySubstrings.java
@@ -0,0 +1,17 @@
+//everytime shift from one value to the other, count the mininum pairs to be form with this current count of a number and the previous count of the other number
+class countBinarySubstrings {
+    public int countBinarySubstrings(String s) {
+        int count = 0;
+        int prev = 0;
+        int curr = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i - 1) == s.charAt(i)) curr++;
+            else {
+                count += Math.min(curr, prev);
+                prev = curr;
+                curr = 1;
+            }
+        }
+        return count += Math.min(curr, prev);
+    }
+}
