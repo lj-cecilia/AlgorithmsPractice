8077da9d4dfc81c387e11642fc3025d9f3737beb
cecilia <yijialiu523@gmail.com>
2023-10-12 21:02:17 -0400

    daily practice

---
+++ src/FindJudgeFromEdges.java
@@ -0,0 +1,19 @@
+class FindJudgeFromEdges {
+    //goal - find judge
+    //judge can trust no one, everyone trusts judge
+    public int findJudge(int n, int[][] trust) {
+        int[] count = new int[n + 1];
+        for (int i = 0; i < trust.length; i++) {
+            int truster = trust[i][0];
+            int trustee = trust[i][1];
+            //deduct count if one trusts others
+            count[truster]--;
+            //increment the count for whom is trusted
+            count[trustee]++;
+        }
+        for (int i = 1; i < n + 1; i++) {
+            if (count[i] == n - 1) return i;
+        }
+        return -1;
+    }
+}
