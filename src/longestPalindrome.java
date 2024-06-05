768916f34dc92f2834bc0696adfdb53bc5037842
cecilia <yijialiu523@gmail.com>
2024-06-05 00:13:58 -0700

    daily practice

--- src/longestPalindrome.java
+++ src/longestPalindrome.java
@@ -1,17 +1,21 @@
+//time O(n)
+//space O(n)
 import java.util.HashSet;
 
 class longestPalindrome {
     public int longestPalindrome(String s) {
         HashSet<Character> set = new HashSet<>();
-        int count = 0;
+        int max = 0;
         for (char ch : s.toCharArray()) {
             if (set.contains(ch)) {
                 set.remove(ch);
-                count += 2;
+                max += 2;
             }
             else set.add(ch);
         }
-        if (set.size() > 0) count += 1;
-        return count;
+        if (set.size() != 0) {
+            max += 1;
+        }
+        return max;
     }
 }
