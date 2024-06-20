620efb58b41ad2432d947e6333fde392c70dae39
cecilia <yijialiu523@gmail.com>
2024-06-19 23:21:54 -0700

    daily practice

---
+++ src/successfulPairs.java
@@ -0,0 +1,30 @@
+//time O(nlongn)
+//space O(n)
+
+
+import java.util.Arrays;
+
+//sort and two pointers
+class successfulPairs {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        int[] result = new int[spells.length];
+        Arrays.sort(potions);
+
+        for (int i = 0; i < spells.length; i++) {
+            int low = 0;
+            int high = potions.length - 1;
+            while (low <= high) {
+                int mid = low + (high - low)/2;
+                long product = (long)potions[mid] * spells[i];
+                if (product < success) {
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 1;
+                }
+            }
+            result[i] = potions.length - low;
+        }
+        return result;
+    }
+}
