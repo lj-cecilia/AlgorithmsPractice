8b356ae9f9ee2985eebc123075336c155fae6fd2
cecilia <yijialiu523@gmail.com>
2024-11-07 00:14:31 -0500

    daily practice

---
+++ src/ReverseLinkedList.java
@@ -0,0 +1,27 @@
+//time O(n)
+//space O(1)
+//for each node, store the next and prev, change its next to prev and move to next one
+class ReverseLinkedList {
+
+//Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode current = head;
+        ListNode prev = null;
+        ListNode next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+}
