661135c90f0a31b8711fc46afacb6769824d3658
cecilia <yijialiu523@gmail.com>
2024-12-15 00:01:00 -0500

    daily practice

--- src/ReorderRoutesToMakeCityZero.java
+++ src/ReorderRoutesToMakeCityZero.java
@@ -1,41 +1,74 @@
 //time O(n)
 //space O(n)
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
-class ReorderRoutesToMakeCityZero {
+//class ReorderRoutesToMakeCityZero {
+//    public int minReorder(int n, int[][] connections) {
+//
+//        boolean[] visited = new boolean[n];
+//        List<List<Integer>> list = new ArrayList<>();
+//        Queue<Integer> q = new LinkedList<>();
+//        int count = 0;
+//
+//        for (int i = 0; i < n; i++) {
+//            list.add(new ArrayList<>());
+//        }
+//
+//        for (int i = 0; i < connections.length; i++) {
+//            list.get(connections[i][0]).add(connections[i][1]);
+//            list.get(connections[i][1]).add(-connections[i][0]);
+//        }
+//
+//        q.offer(0);
+//        visited[0] = true;
+//
+//        while (!q.isEmpty()) {
+//            int current = q.remove();
+//            for (int vertex : list.get(Math.abs(current))) {
+//                if (!visited[Math.abs(vertex)]) {
+//                    q.add(vertex);
+//                    visited[Math.abs(vertex)] = true;
+//                    if (vertex > 0) count++;
+//                }
+//            }
+//        }
+//        return count;
+//    }
+//}
+
+
+//memory limit exceeded with adjacency matrix
+class ReorderRoutesToMakeCityZeroÏ {
     public int minReorder(int n, int[][] connections) {
-
-        boolean[] visited = new boolean[n];
-        List<List<Integer>> list = new ArrayList<>();
-        Queue<Integer> q = new LinkedList<>();
+        int[][] bi = new int[n][n];
+        int[][] one = new int[n][n];
         int count = 0;
 
-        for (int i = 0; i < n; i++) {
-            list.add(new ArrayList<>());
+        for (int[] connection : connections) {
+            int start = connection[0];
+            int end = connection[1];
+            bi[start][end] = 1;
+            bi[end][start] = 1;
+            one[start][end] = 1;
         }
 
-        for (int i = 0; i < connections.length; i++) {
-            list.get(connections[i][0]).add(connections[i][1]);
-            list.get(connections[i][1]).add(-connections[i][0]);
-        }
-
-        q.offer(0);
-        visited[0] = true;
+        HashSet<Integer> visited = new HashSet<>();
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        visited.add(0);
 
         while (!q.isEmpty()) {
             int current = q.remove();
-            for (int vertex : list.get(Math.abs(current))) {
-                if (!visited[Math.abs(vertex)]) {
-                    q.add(vertex);
-                    visited[Math.abs(vertex)] = true;
-                    if (vertex > 0) count++;
+            for (int i = 0; i < n; i++) {
+                if (!visited.contains(i) && (bi[current][i] == 1)) {
+                    q.add(i);
+                    visited.add(i);
+                    // if (one[current][i] == 1 && one[i][current] == 0) count++;
+                    if (one[current][i] == 1) count++;
                 }
             }
         }
         return count;
-
     }
 }
+
--- src/longestZigZagInBinaryTree.java
+++ src/longestZigZagInBinaryTree.java
@@ -1,60 +1,60 @@
-//time O(n)
-//space O(n)
-//visit every node once for n and space n for the call stack of linkedlist like treepublic class longestZigZagInBinaryTree {
-     public class TreeNode {
-         int val;TreeNode left;
-         TreeNode right;
-         TreeNode() {};
-         TreeNode(int val) {
-             this.val = val;
-         }
-         TreeNode(int val, TreeNode left, TreeNode right) {
-             this.val = val;
-             this.left = left;
-             this.right = right;
-         }
-     }
-     int max = 0;
-     public int longestZigZag(TreeNode root) {
-         if (root == null) return 0;
-         dfs(root.left, 1, true);
-         dfs(root.right, 1, false);
-         return max;
-     }
-
-     public void dfs(TreeNode root, int steps, boolean isLeft) {
-         if (root == null) return;
-         max = Math.max(max, steps);
-         if (isLeft) {
-             dfs(root.left, 1, true);
-             dfs(root.right, steps + 1, false);
-         }
-         else {
-             dfs(root.left, steps + 1, true);
-             dfs(root.right, 1, false);
-         }
-     }
-}
-
-//abbreviated version with one recursive call from the parent function
-//class Solution {
-//    int max = 0;
-//    public int longestZigZag(TreeNode root) {
-//        boolean isLeft = false;
-//        dfs(root, isLeft, 0);
-//        return max;
-//    }
+////time O(n)
+////space O(n)
+////visit every node once for n and space n for the call stack of linkedlist like treepublic class longestZigZagInBinaryTree {
+//     public class TreeNode {
+//         int val;TreeNode left;
+//         TreeNode right;
+//         TreeNode() {};
+//         TreeNode(int val) {
+//             this.val = val;
+//         }
+//         TreeNode(int val, TreeNode left, TreeNode right) {
+//             this.val = val;
+//             this.left = left;
+//             this.right = right;
+//         }
+//     }
+//     int max = 0;
+//     public int longestZigZag(TreeNode root) {
+//         if (root == null) return 0;
+//         dfs(root.left, 1, true);
+//         dfs(root.right, 1, false);
+//         return max;
+//     }
 //
-//    public void dfs(TreeNode root, boolean isLeft, int current) {
-//        if (root == null) return;
-//        max = Math.max(max, current);
-//        if (isLeft) {
-//            dfs(root.left, true, 1);
-//            dfs(root.right, false, current + 1);
-//        }
-//        else {
-//            dfs(root.left, true, current + 1);
-//            dfs(root.right, false, 1);
-//        }
-//    }
+//     public void dfs(TreeNode root, int steps, boolean isLeft) {
+//         if (root == null) return;
+//         max = Math.max(max, steps);
+//         if (isLeft) {
+//             dfs(root.left, 1, true);
+//             dfs(root.right, steps + 1, false);
+//         }
+//         else {
+//             dfs(root.left, steps + 1, true);
+//             dfs(root.right, 1, false);
+//         }
+//     }
 //}
+//
+////abbreviated version with one recursive call from the parent function
+////class Solution {
+////    int max = 0;
+////    public int longestZigZag(TreeNode root) {
+////        boolean isLeft = false;
+////        dfs(root, isLeft, 0);
+////        return max;
+////    }
+////
+////    public void dfs(TreeNode root, boolean isLeft, int current) {
+////        if (root == null) return;
+////        max = Math.max(max, current);
+////        if (isLeft) {
+////            dfs(root.left, true, 1);
+////            dfs(root.right, false, current + 1);
+////        }
+////        else {
+////            dfs(root.left, true, current + 1);
+////            dfs(root.right, false, 1);
+////        }
+////    }
+////}
